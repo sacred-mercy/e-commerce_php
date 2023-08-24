5ec5f5946f24d08dd908d612d5042d930f664db2
gauravnegi9634@gmail.com <gauravnegi9634@gmail.com>
2023-08-24 18:12:53 +0530

    razor pay credentials integrated to env.php

---
+++ razorpay-php/config.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once dirname(__DIR__) . '/env.php';
+
+$keyId = $RAZORPAY_KEY_ID;
+$keySecret = $RAZORPAY_KEY_SECRET;
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
