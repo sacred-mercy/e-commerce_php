867a5670cad84ea98416508331c1fd8efd7d4f4d
gauravnegi9634@gmail.com <gauravnegi9634@gmail.com>
2023-08-29 11:51:37 +0530

    improved admin dashboard db queries

--- models/adminModel.php
+++ models/adminModel.php
@@ -8,7 +8,7 @@ class AdminModel
     function getDashboardData()
     {
         try {
-            $totalOrders = pg_query($GLOBALS['db'], "SELECT COUNT(*) FROM orders");
+            $totalOrders = pg_query($GLOBALS['db'], "SELECT COUNT(*) FROM orders WHERE status != 'cancelled'");
             $totalOrders = pg_fetch_row($totalOrders)[0];
 
             $totalProducts = pg_query($GLOBALS['db'], "SELECT COUNT(*) FROM products");
@@ -17,13 +17,13 @@ class AdminModel
             $totalUsers = pg_query($GLOBALS['db'], "SELECT COUNT(*) FROM users");
             $totalUsers = pg_fetch_row($totalUsers)[0];
 
-            $totalRevenue = pg_query($GLOBALS['db'], "SELECT SUM(price) FROM orders");
+            $totalRevenue = pg_query($GLOBALS['db'], "SELECT SUM(price) FROM orders WHERE status != 'cancelled'");
             $totalRevenue = pg_fetch_row($totalRevenue)[0];
 
-            $todayOrders = pg_query($GLOBALS['db'], "SELECT COUNT(*) FROM orders WHERE date_time >= CURRENT_DATE");
+            $todayOrders = pg_query($GLOBALS['db'], "SELECT COUNT(*) FROM orders WHERE date_time >= CURRENT_DATE AND status != 'cancelled'");
             $todayOrders = pg_fetch_row($todayOrders)[0];
 
-            $todayRevenue = pg_query($GLOBALS['db'], "SELECT SUM(price) FROM orders WHERE date_time >= CURRENT_DATE");
+            $todayRevenue = pg_query($GLOBALS['db'], "SELECT SUM(price) FROM orders WHERE date_time >= CURRENT_DATE AND status != 'cancelled'");
             $todayRevenue = pg_fetch_row($todayRevenue)[0];
 
             return array(
@@ -51,9 +51,10 @@ class AdminModel
             $query = "SELECT p.title, COUNT(*) * SUM(od.quantity) AS product_count
             FROM orders AS o
             JOIN orders_detail AS od ON o.id = od.order_id
-            JOIN products AS p ON p.id = od.product_id"
-                . (($timePeriod === 'lifetime') ? "" : " WHERE o.date_time >= DATE_TRUNC('" . $timePeriod . "', CURRENT_DATE)")
-                . " GROUP BY p.title
+            JOIN products AS p ON p.id = od.product_id WHERE "
+                . (($timePeriod === 'lifetime') ? "" : "o.date_time >= DATE_TRUNC('" . $timePeriod . "', CURRENT_DATE) AND")
+                . " o.status != 'cancelled'
+                    GROUP BY p.title
                     ORDER BY product_count DESC
                     LIMIT 5";
 
